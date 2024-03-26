d175e7f2a4519c795f72722788835622ec847c34
Estevaosouza-oss <estevaokayes8@gmail.com>
2024-03-26 12:13:47 -0300

    Calcula o numero real
    transforma o numero real em inteiro

---
+++ Numero_Inteiro.py
@@ -0,0 +1,4 @@
+from math import floor
+RealNumber = float(input("Me Digite um Numero Real Utilizando o . : "))
+print('O seu Sumero Escolhido foi o ' +str(RealNumber))
+print('Este numero em inteiro é {}'.format(floor(RealNumber)))
