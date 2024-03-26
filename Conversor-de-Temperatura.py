5f33d4f2db9e77a562ab69bff6baf211421d2394
Estevaosouza-oss <estevaokayes8@gmail.com>
2024-03-26 11:19:52 -0300

    Conversor de ºC para ºF
    Nesse codigo, fiz um conversor de temperatuda de ºC para ºF

---
+++ Conversor-de-Temperatura.py
@@ -0,0 +1,4 @@
+celcius = float(input('Digite a Temperatura em ºC : '))
+Fahrenheit = celcius * 1.8 + 32
+print('A Temperatura Atual é de Aproximadamente {:.2f}ºC'.format(celcius))
+print('{:.2f}ºC em Fahreinheit é de {:.2f}ºF'.format(celcius, Fahrenheit))
